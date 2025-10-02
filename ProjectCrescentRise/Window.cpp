6f5cdd3d0e5f02f9171df61032bbed85a3ce3f8a
(Student  C00283152) David Strikaitis <C00283152@setu.ie>
2025-10-02 12:12:16 +0100

    added some rendering of triangles

--- ProjectCrescentRise/Window.cpp
+++ ProjectCrescentRise/Window.cpp
@@ -24,9 +24,56 @@ bool Window::windowClosed()
 
 void Window::render()
 {
+    float points[] = {
+   0.0f,  0.5f,  0.0f,
+   0.5f, -0.5f,  0.0f,
+  -0.5f, -0.5f,  0.0f
+    };
+    GLuint vbo = 0;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
+
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    const char* vertex_shader =
+        "#version 410 core\n"
+        "in vec3 vp;"
+        "void main() {"
+        "  gl_Position = vec4( vp, 1.0 );"
+        "}";
+    const char* fragment_shader =
+        "#version 410 core\n"
+        "out vec4 frag_colour;"
+        "void main() {"
+        "  frag_colour = vec4( 0.5, 0.0, 0.5, 1.0 );"
+        "}";
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertex_shader, NULL);
+    glCompileShader(vs);
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragment_shader, NULL);
+    glCompileShader(fs);
+
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, fs);
+    glAttachShader(shader_program, vs);
+    glLinkProgram(shader_program);
     /* Render here */
     glClear(GL_COLOR_BUFFER_BIT);
-    
+
+    // Put the shader program, and the VAO, in focus in OpenGL's state machine.
+    glUseProgram(shader_program);
+ //   glBindVertexArray(vao);
+ 
+    // Draw points 0-3 from the currently bound VAO with current in-use shader.
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
     /* Swap front and back buffers */
     glfwSwapBuffers(m_window);
 
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Planet hopping Rogue Like!
+# Issues Encountered will be in this Doc
+
+## Early stages (render pipeline and before)
+
+- creation of the ECS model mimicing, by using arrays of functions
+- Update function has been abstracted into a different class that contains an array of the different functions.
