943a03159e37016df37680e48f1cf78d04ed747f
DavidStrikaitis <davidstrik282@gmail.com>
2025-10-22 16:17:47 +0100

    added vornoi generation

--- ProjectCrescentRise/GroundTile.h
+++ ProjectCrescentRise/GroundTile.h
@@ -8,6 +8,15 @@ public:
 	virtual void Start()override;
 	virtual void Render()override;
 private:
+	void generateVornoi(std::vector<std::vector<float>> &t_heights, int width);
+
+	int pointAmountVornoi = 256;
+	float scaleFactor = 0.1f;
+	int width = 128;
+	float disp = 1.f;
+	float tileSize = 0.5f;
+	float heightFactor = 0.1f;
+
 	const unsigned int pointLength = 20;
 	const unsigned int hop = 3;
 	int size = 0;
